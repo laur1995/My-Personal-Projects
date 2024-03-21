eba0619f13e03ac2848bd6a949cb648565ceba7d
laur1995 <laurentiulao100@gmail.com>
2024-03-21 04:55:01 +0200

    My Online Guitar Shop Project

--- Guitar-shop/script.js
+++ Guitar-shop/script.js
@@ -1,5 +1,12 @@
-const header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", window.scrollY > 60);
-});
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
