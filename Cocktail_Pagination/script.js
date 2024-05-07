6e6d60f76d64c1b708f8361cc466f38a9b860db6
laur1995 <laurentiulao100@gmail.com>
2024-05-07 19:00:38 +0300

    Cocktail Menu List (Pagination in JS)

---
+++ Cocktail_Pagination/script.js
@@ -0,0 +1,46 @@
+let thisPage = 1;
+let limit = 6;
+let list = document.querySelectorAll(".list .item");
+
+function loadItem() {
+  let beginGet = limit * (thisPage - 1);
+  let endGet = limit * thisPage - 1;
+  list.forEach((item, key) => {
+    if (key >= beginGet && key <= endGet) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+  listPage();
+}
+
+loadItem();
+
+function listPage() {
+  let count = Math.ceil(list.length / limit);
+  let listPageElement = document.querySelector(".listPage");
+  listPageElement.innerHTML = "";
+
+  if (thisPage > 1) {
+    let prev = document.createElement("li");
+    prev.innerText = "PREV";
+    prev.setAttribute("onclick", "changePage(" + (thisPage - 1) + ")");
+    listPageElement.appendChild(prev);
+  }
+
+  for (let i = 1; i <= count; i++) {
+    let newPage = document.createElement("li");
+    newPage.innerText = i;
+    if (i == thisPage) {
+      newPage.classList.add("active");
+    }
+    newPage.setAttribute("onclick", "changePage(" + i + ")");
+    listPageElement.appendChild(newPage);
+  }
+}
+
+function changePage(i) {
+  thisPage = i;
+  loadItem();
+}
